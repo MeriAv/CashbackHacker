759c9774bb1656dbafb52eab84aeb4fe588c0380
MeriAv <mari.avdeeva1407@gmal.com>
2021-01-27 14:43:48 +0300

    add testing

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -1,33 +1,33 @@
 package ru.netology.domain;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackerTest {
 
-    @Test
+    @org.testng.annotations.Test
     void snouldIfTheAmountIsTheNormal() {
        CashbackHacker cashbackHacker = new CashbackHacker();
        int actual = cashbackHacker.remain(2300);
        int expected = 700;
-       assertEquals(expected, actual);
+       assertEquals(actual, expected);
     }
 
-    @Test
+    @org.testng.annotations.Test
     void snouldIfTheAmountLessThanTheNorm() {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int actual = cashbackHacker.remain(200);
         int expected = 800;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
-    @Test
+    @org.testng.annotations.Test
     void snouldIfTheAmountIsEqualToTheMinimum() {
         CashbackHacker cashbackHacker = new CashbackHacker();
         int actual = cashbackHacker.remain(1000);
         int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
 
